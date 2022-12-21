a31a398e19e3fc286c429e5eadf45de1f55873a8
SKYlake233 <72776531+SKYlake233@users.noreply.github.com>
2022-12-21 17:57:42 +0800

    Add files via upload

--- ks.sql
+++ ks.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80027
  File Encoding         : 65001
 
- Date: 19/12/2022 19:07:38
+ Date: 21/12/2022 17:57:14
 */
 
 SET NAMES utf8mb4;
@@ -42,16 +42,17 @@ INSERT INTO `admin_user` VALUES (1, 'wc', 'wc', '王创', '10012341234', '翻斗
 DROP TABLE IF EXISTS `alarm`;
 CREATE TABLE `alarm`  (
   `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '报警id',
-  `data_index` bigint(0) NOT NULL COMMENT '报警记录',
-  `read` int(0) NOT NULL DEFAULT 0 COMMENT '是否已经已读',
+  `data_index` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '报警记录',
+  `is_read` int(0) NOT NULL DEFAULT 0 COMMENT '是否已经已读',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_al_data`(`data_index`) USING BTREE,
-  CONSTRAINT `fk_al_data` FOREIGN KEY (`data_index`) REFERENCES `data` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+  INDEX `fk_al_data`(`data_index`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of alarm
 -- ----------------------------
+INSERT INTO `alarm` VALUES (6, '当前时间2022-12-21 17:21:31   NO2超标，数值为85', 0);
+INSERT INTO `alarm` VALUES (7, '当前时间2022-12-21 17:22:04   NO2超标，数值为87   SO2超标，数值为17', 0);
 
 -- ----------------------------
 -- Table structure for alarm_rule
@@ -69,6 +70,7 @@ CREATE TABLE `alarm_rule`  (
 -- ----------------------------
 -- Records of alarm_rule
 -- ----------------------------
+INSERT INTO `alarm_rule` VALUES (40, 0.85, 80, 1, 80, 15);
 
 -- ----------------------------
 -- Table structure for data
@@ -80,11 +82,11 @@ CREATE TABLE `data`  (
   `collect_location` bigint(0) NOT NULL COMMENT '采集地点',
   `collect_time` datetime(0) NOT NULL COMMENT '采集时间',
   `temperature` float NULL DEFAULT NULL COMMENT '温度值',
-  `humidity` float NULL DEFAULT NULL COMMENT '湿度值',
-  `PM25` float NULL DEFAULT NULL COMMENT 'pm2.5',
+  `humidity` int(0) NULL DEFAULT NULL COMMENT '湿度值',
+  `PM25` int(0) NULL DEFAULT NULL COMMENT 'pm2.5',
   `CO` float NULL DEFAULT NULL COMMENT '一氧化碳',
-  `NO2` float NULL DEFAULT NULL COMMENT '二氧化氮',
-  `SO2` float NULL DEFAULT NULL COMMENT '二氧化硫',
+  `NO2` int(0) NULL DEFAULT NULL COMMENT '二氧化氮',
+  `SO2` int(0) NULL DEFAULT NULL COMMENT '二氧化硫',
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `fk_co_device`(`collect_device`) USING BTREE,
   INDEX `fk_co_location`(`collect_location`) USING BTREE,
@@ -95,6 +97,9 @@ CREATE TABLE `data`  (
 -- ----------------------------
 -- Records of data
 -- ----------------------------
+INSERT INTO `data` VALUES (25, 25, 1, '2022-12-21 09:21:27', 22.5, 1, 13, NULL, 85, 2);
+INSERT INTO `data` VALUES (26, 25, 1, '2022-12-21 09:22:04', 33.9, 0, 21, NULL, 87, 17);
+INSERT INTO `data` VALUES (27, 25, 1, '2022-12-21 17:27:50', 34.5, 1, 16, 0.12, 65, 9);
 
 -- ----------------------------
 -- Table structure for device_info
@@ -118,7 +123,7 @@ CREATE TABLE `device_info`  (
 -- ----------------------------
 -- Records of device_info
 -- ----------------------------
-INSERT INTO `device_info` VALUES (1, 'A设备', 'A厂商', 2, 2, '支持', '支持', '支持', '支持', '支持', '支持');
+INSERT INTO `device_info` VALUES (2, 'A设备', 'A厂商', 2, 4, '支持', '支持', '支持', '支持', '支持', '支持');
 
 -- ----------------------------
 -- Table structure for device_item
@@ -136,11 +141,15 @@ CREATE TABLE `device_item`  (
   INDEX `fk_location`(`install_location`) USING BTREE,
   CONSTRAINT `fk_device` FOREIGN KEY (`device_cate`) REFERENCES `device_info` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `fk_location` FOREIGN KEY (`install_location`) REFERENCES `place` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of device_item
 -- ----------------------------
+INSERT INTO `device_item` VALUES (25, 2, 1, 1, '2022-12-21 08:52:50', '2024-12-21 08:52:50');
+INSERT INTO `device_item` VALUES (26, 2, 1, 2, '2022-12-21 08:52:57', '2024-12-21 08:52:57');
+INSERT INTO `device_item` VALUES (27, 2, 1, 3, '2022-12-21 08:53:03', '2024-12-21 08:53:03');
+INSERT INTO `device_item` VALUES (28, 2, 1, 4, '2022-12-21 08:53:08', '2024-12-21 08:53:08');
 
 -- ----------------------------
 -- Table structure for place
@@ -152,12 +161,15 @@ CREATE TABLE `place`  (
   `latitude` double NOT NULL COMMENT '地点维度',
   `place_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '地点描述',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of place
 -- ----------------------------
-INSERT INTO `place` VALUES (1, 112.470572, 34.624854, '洛龙区市政府');
+INSERT INTO `place` VALUES (1, 112.460223, 34.626339, '洛龙区市政府');
+INSERT INTO `place` VALUES (2, 112.402427, 34.663965, '涧西区政府');
+INSERT INTO `place` VALUES (3, 112.43449520685112, 34.66619363154539, '西工区政府');
+INSERT INTO `place` VALUES (4, 112.47556627375056, 34.68964220501026, '老城区政府');
 
 -- ----------------------------
 -- Table structure for user
@@ -170,7 +182,7 @@ CREATE TABLE `user`  (
   `nick_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户昵称',
   `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '用户邮箱，用来联系',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
