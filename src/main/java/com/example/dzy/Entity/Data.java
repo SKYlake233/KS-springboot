d61ca9caddf68f6a01d07a928d7444722ee1ef9b
SKYlake233 <72776531+SKYlake233@users.noreply.github.com>
2022-12-21 19:13:39 +0800

    bug

--- src/main/java/com/example/dzy/Entity/Data.java
+++ src/main/java/com/example/dzy/Entity/Data.java
@@ -1,11 +1,14 @@
 package com.example.dzy.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @lombok.Data
 public class Data {
 
   private long id;
   private long collectDevice;
   private long collectLocation;
+  @JsonFormat(timezone = "GMT+8",pattern = "yyyy年MM月dd日 HH:mm")
   private java.sql.Timestamp collectTime;
   private Double temperature;
   private Double humidity;
